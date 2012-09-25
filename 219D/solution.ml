237f7f2deac73ca6e1b65babab9b881bbe4a2db4
Mikhail Mandrykin <mandrykin@ispras.ru>
2012-09-25 16:03:48 +0400

    Problem 219D (version 2)

--- 219D/solution.ml
+++ 219D/solution.ml
@@ -1,28 +1,21 @@
 
 let read format f = Scanf.bscanf Scanf.Scanning.stdib format f
 
-module L = struct
-  let iter lst f = List.iter f lst
-  let map lst f = List.map f lst
-end
-
-module A = struct
-  let iteri f a = Array.iteri a f
-end
-
-let bfs graph n start f =
-  let flags = Array.make n false in
+let bfs graph start f =
+  let flags = Array.make (Array.length graph) false in
   let q = Queue.create () in
+  let fold_vertex result v =
+    List.fold_left (fun result (i, dir) ->
+      if not flags.(i) then (Queue.add i q; flags.(i) <- true; f result v i dir)
+      else result) result graph.(v)
+  in
+  let rec main_loop result = match Queue.is_empty q with
+    | true -> result
+    | false -> main_loop (fold_vertex result (Queue.take q))
+  in
   Queue.add start q;
   flags.(start) <- true;
-  while not (Queue.is_empty q) do
-    let v = Queue.take q in
-    L.iter graph.(v) (fun (i, dir) ->
-      if not flags.(i) then
-        (Queue.add i q;
-         f v i dir;
-         flags.(i) <- true))
-  done
+  main_loop
 
 let () =
   let n = read "%d\n" (fun x -> x) in
@@ -32,15 +25,14 @@ let () =
     graph.(f) <- (t, true) :: graph.(f);
     graph.(t) <- (f, false) :: graph.(t)
   done;
-  let sum = ref 0 in
-  bfs graph n 0 (fun _ _ dir -> if not dir then incr sum);
-  let sums = Array.make n !sum in
-  let min_sum = ref !sum in
-  bfs graph n 0 (fun f t dir ->
+  let sum = bfs graph 0 (fun sum _ _ dir -> if dir then sum else sum + 1) 0 in
+  let sums = Array.make n sum in
+  let min_sum = bfs graph 0 (fun min_sum f t dir ->
     sums.(t) <- sums.(f) + (if dir then 1 else -1);
-    min_sum := min !min_sum sums.(t));
-  print_endline (string_of_int !min_sum);
+    min min_sum sums.(t)) sum
+  in
+  print_endline (string_of_int min_sum);
   let b = Buffer.create (10 * n) in
-  A.iteri sums (fun i s ->
-    if s = !min_sum then Buffer.add_string b (string_of_int (i + 1) ^ " "));
+  Array.iteri (fun i s ->
+    if s = min_sum then Buffer.add_string b (string_of_int (i + 1) ^ " ")) sums;
   print_endline (Buffer.sub b 0 (Buffer.length b - 1))
