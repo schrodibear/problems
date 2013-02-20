2202b4ab6795ea47aafc53aa6ec9a1dd67f04017
Mikhail Mandrykin <mandrykin@ispras.ru>
2013-02-20 12:45:19 +0400

    227B: int64 used

--- 227B/solution.ml
+++ 227B/solution.ml
@@ -3,21 +3,24 @@ let read format f = Scanf.bscanf Scanf.Scanning.stdib format f
 
 let () =
   let n = read "%d\n" (fun x -> x) in
-  let nowhere = -1 in
-  let lefts = Array.make n nowhere in
-  let rights = Array.make n nowhere in
+  let lefts = Array.make n None in
+  let rights = Array.make n None in
   for i = 1 to n do
     let a = read "%d " (fun x -> x - 1) in
-    if lefts.(a) = nowhere then lefts.(a) <- i;
-    rights.(a) <- n - i + 1
+    if lefts.(a) = None then lefts.(a) <- Some i;
+    rights.(a) <- Some (n - i + 1)
   done;
-  let left = ref (Int64.of_int 0) in
-  let right = ref (Int64.of_int 0) in
+  let some64 = function
+    | Some x -> Int64.of_int x
+    | None -> failwith "some64: None"
+  in
+  let left = ref 0L in
+  let right = ref 0L in
   let m = read "%d\n" (fun x -> x) in
   for i = 1 to m do
     let q = read " %d" (fun x -> x - 1) in
-    left := Int64.add !left (Int64.of_int lefts.(q));
-    right := Int64.add !right (Int64.of_int rights.(q))
+    left := Int64.(add !left (some64 lefts.(q)));
+    right := Int64.(add !right (some64 rights.(q)))
   done;
-  Printf.printf "%Ld %Ld\n" !left !right 
-  
+  Printf.printf "%Ld %Ld\n" !left !right
+
